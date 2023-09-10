a17649add6f5271280901a43d87a1b0576c86c7b
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-09-11 00:41:54 +0530

    char field update

--- sentiment_analysis/serializer.py
+++ sentiment_analysis/serializer.py
@@ -2,4 +2,4 @@ from rest_framework import serializers
 
 
 class SentimentAnalysisSerializer(serializers.Serializer):
-    sentence = serializers.CharField(max_length=200)
+    sentence = serializers.CharField()

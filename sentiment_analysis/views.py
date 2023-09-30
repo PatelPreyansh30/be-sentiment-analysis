d4c7673dab46e8df3d1c7709b36bedf0e4cbec9b
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-09-30 11:43:43 +0530

    code refactor

--- sentiment_analysis/views.py
+++ sentiment_analysis/views.py
@@ -1,43 +1,17 @@
-import numpy as np
+# from joblib import load
 from transformers import TFRobertaForSequenceClassification, RobertaTokenizer
 from rest_framework import views, response, status
-from . import serializer
+from . import serializer, vector_converstion
 
 # Set your model here
 model_path = "./model/First_model"
 tokenizer_path = "./model/First_model_tokenizer"
 
 print("MODEL AND TOKENIZER GLOBALLY IMPORT STARTING")
+# sahil = load("./model/sentiment_analysis.joblib")
 model = TFRobertaForSequenceClassification.from_pretrained(model_path)
 tokenizer = RobertaTokenizer.from_pretrained(tokenizer_path)
 print("MODEL AND TOKENIZER GLOBALLY IMPORT ENDING")
-
-
-def tokenizer_analysis(data):
-    # Convert to string
-    data = str(data)
-
-    # Convert string to data matrix
-    data_encodings = tokenizer(
-        data, padding=True, truncation=True, return_tensors="tf", max_length=128)
-    input_ids = data_encodings["input_ids"]
-    attention_mask = data_encodings["attention_mask"]
-
-    # Predict the sentence
-    predictions = model.predict(
-        {"input_ids": input_ids, "attention_mask": attention_mask})
-    predicted_label = np.argmax(predictions[0])
-    logits = predictions.logits[0]
-    probabilities = np.exp(logits) / np.sum(np.exp(logits))
-    scaled_probabilities = probabilities * 100
-
-    if predicted_label == 0:
-        output = "Negative"
-    elif predicted_label == 1:
-        output = "Neutral"
-    else:
-        output = "Positive"
-    return {"type": output, "scaled_probability": scaled_probabilities[predicted_label]}
 
 
 class SentimentAnalysisView(views.APIView):
@@ -45,6 +19,11 @@ class SentimentAnalysisView(views.APIView):
         serializer_data = serializer.SentimentAnalysisSerializer(
             data=request.data)
         if serializer_data.is_valid():
-            result = tokenizer_analysis(serializer_data.data.get('sentence'))
+            sentence = serializer_data.data.get('sentence')
+            result = vector_converstion.tokenizer_analysis(sentence, model, tokenizer)
+            # vector = vector_converstion.naive_byse_classification(
+            #     serializer_data.data.get('sentence'))
+            # print(vector)
+            # result = sahil.predict(vector)
             return response.Response({"status": "success", "result": result}, status.HTTP_201_CREATED)
         return response.Response({"status": "error"}, status.HTTP_400_BAD_REQUEST)
